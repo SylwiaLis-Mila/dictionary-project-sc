d4bf2c2e5bd86ef56b1bc896c19280871f319e82
SylwiaLis-Mila <sylwialismila@gmail.com>
2021-05-19 11:01:51 +0200

    add search engine

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.png';
 import './App.css';
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
@@ -8,8 +9,15 @@ function App() {
         <img src={logo}
         className="App-logo"
         alt="logo" />
-  
       </header>
+      <main>
+        <Dictionary />
+
+      </main>
+      <footer className="text-center">
+
+        Coded by Sylwia Lis-Mila
+      </footer>
     </div>
   );
 }
